81feb195d600534a019f32a574470b529b52b9e1
Tony Blum <tonyblum@me.com>
2020-11-20 08:22:17 -0800

    routes3

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'users#sign_in'
+  root 'welcome#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
