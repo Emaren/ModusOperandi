9c5735ad81804a59928811259945640d7d900552
Tony Blum <tonyblum@me.com>
2020-11-25 06:41:46 -0800

    Project/Tasks added

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_21_135826) do
+ActiveRecord::Schema.define(version: 2020_11_21_144258) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "uuid-ossp"
 
   create_table "projects", force: :cascade do |t|
     t.string "name"
@@ -47,5 +48,4 @@ ActiveRecord::Schema.define(version: 2020_11_21_135826) do
   end
 
   add_foreign_key "projects", "users"
-  add_foreign_key "tasks", "projects"
 end
