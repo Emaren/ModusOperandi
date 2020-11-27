8835f9f9b34c9cffec18b8667e383c17226146d7
Tony Blum <tonyblum@me.com>
2020-11-27 06:52:58 -0800

    welcome/index

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   devise_for :users
   # devise_for :users
   root to: 'projects#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'welcome/index'
 
 
 end
