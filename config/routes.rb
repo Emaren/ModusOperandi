601a7b18541851d0032e29dc2f3193450fa1a052
Tony Blum <tonyblum@me.com>
2020-11-27 06:32:53 -0800

    add heroku remotes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  resources :projects do
+    resources :tasks
+  end
   devise_for :users
-  resources :projects
   # devise_for :users
-  root to: 'welcome#index'
+  root to: 'projects#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 
